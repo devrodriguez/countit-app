2925e3b23dcc94e5858524a620fffeacb302e47b
John Rodriguez <jrodriguez@192.168.81.11>
2024-11-10 18:06:41 -0500

    add count bunches

--- src/app/pages/count-bunches/count-bunches.page.ts
+++ src/app/pages/count-bunches/count-bunches.page.ts
@@ -1,6 +1,6 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { LoadingController, ToastController } from '@ionic/angular';
-import jsQR from 'jsqr';
+import { Html5Qrcode } from "html5-qrcode";
 import { CountsService } from 'src/app/services/counts.service';
 
 @Component({
@@ -9,132 +9,46 @@ import { CountsService } from 'src/app/services/counts.service';
   styleUrls: ['./count-bunches.page.scss'],
 })
 export class CountBunchesPage implements OnInit {
-  @ViewChild('video', { static: false }) video: ElementRef | undefined;
-  @ViewChild('canvas', { static: false }) canvas: ElementRef | undefined;
-
-
-  loadingElement: HTMLIonLoadingElement | undefined;
   private beepAudio: HTMLAudioElement | undefined;
 
   isScanActive = false;
   itsScanning = '';
-  scanResult = '';
-  videoElement: any;
-  canvasElement: any;
-  canvasContext: any;
   collector: any;
   product: any;
   amount: any;
+  html5QrCode: any;
 
   constructor(private toastCtrl: ToastController, private loadingCtrl: LoadingController, private countsSrv: CountsService) {
     this.beepAudio = new Audio('../../../assets/sounds/beep.mp3');
   }
 
-  ngAfterViewInit() {
-    this.videoElement = this.video?.nativeElement;
-    this.canvasElement = this.canvas?.nativeElement;
-    this.canvasContext = this.canvasElement.getContext('2d');
-  }
-
   ngOnInit() {
+    this.html5QrCode = new Html5Qrcode("qr-reader");
   }
 
-  async prepareScanner() {
-    const isCameraAvailable = await this.checkCamera();
-  }
-
-  async checkCamera() {
-    const cameraPermission = await navigator.permissions.query({ name: 'camera' } as any);
-
-    const isCameraOk = cameraPermission.state !== 'denied';
-    const hasMediaDevice = 'mediaDevices' in navigator;
-    const hasUserMedia = 'getUserMedia' in navigator.mediaDevices;
-
-    if (!hasMediaDevice) {
-      this.showToast('El dispositivo no puede leer codigos');
-      return;
-    }
-
-    if (!hasUserMedia) {
-      this.showToast('El dispositivo no esta listo');
-      return;
-    }
-
-    return isCameraOk;
-  }
-
-  async scan(elementScanned: string) {
+  async scanQR(elementScanned: string) {
     this.itsScanning = elementScanned;
-
-    const stream = await navigator.mediaDevices.getUserMedia({
-      video: {
-        facingMode: 'environment', 
-        width: 320,
-        height: 240
-      },
-    });
-
-    this.videoElement.srcObject = stream;
-    this.videoElement.play();
     this.isScanActive = true;
 
-    this.loadingElement = await this.loadingCtrl.create({});
-    await this.loadingElement.present()
-
-    requestAnimationFrame(this.scanHandler.bind(this));
-  }
-
-  async scanHandler() {
-    if (this.videoElement.readyState === this.videoElement.HAVE_ENOUGH_DATA) {
-      if (this.loadingElement) {
-        await this.loadingElement.dismiss();
-        this.loadingElement = undefined;
-        this.isScanActive = true;
+    const qrConfig = {
+      fps: 20,
+      qrbox: {
+        width: 200,
+        height: 200
       }
+    };
 
-      this.canvasElement.height = this.videoElement.videoHeight;
-      this.canvasElement.width = this.videoElement.width;
-
-      this.canvasContext.drawImage(
-        this.videoElement,
-        0,
-        0,
-        this.canvasElement.width,
-        this.canvasElement.height
-      );
-
-      // Get image data
-      const imageData = this.canvasContext.getImageData(
-        0,
-        0,
-        this.canvasElement.width,
-        this.canvasElement.height
-      );
-
-      const code = jsQR(imageData.data, imageData.width, imageData.height, {
-        inversionAttempts: 'dontInvert',
-      });
-
-      console.log(code);
-
-      if (code) {
-        this.isScanActive = false;
-        this.scanResult = code.data;
-        //this.showToast(`Read ${this.scanResult}`);
-        this.setScannedData(code.data);
-
-        //this.beepAudio.currentTime = 0;
+    await this.html5QrCode.start(
+      {
+        facingMode: "environment",
+      },
+      qrConfig,
+      async (decodedText: any, decodedResult: any) => {
+        this.setScannedData(decodedText);
         this.beepAudio?.play();
-
-      } else {
-        if (this.isScanActive) {
-          requestAnimationFrame(this.scanHandler.bind(this));
-        }
-      }
-
-    } else {
-      requestAnimationFrame(this.scanHandler.bind(this));
-    }
+        await this.html5QrCode.stop();
+        this.isScanActive = false;
+      })
   }
 
   setScannedData(data: string) {
@@ -151,11 +65,12 @@ export class CountBunchesPage implements OnInit {
     }
   }
 
-  reset() {
+  async reset() {
     this.isScanActive = false;
     this.amount = null;
     this.itsScanning = '';
     this.setScannedData('');
+    await this.html5QrCode.stop();
   }
 
   async save() {
